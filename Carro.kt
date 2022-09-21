b91631f85e642f116cbff84b48acb033b0407455
luanaflorencio <luanaflorenciopc@gmail.com>
2022-09-20 22:58:44 -0300

    Adicionando função

--- Carro.kt
+++ Carro.kt
@@ -1,2 +1,24 @@
 class Carro {
+    val marca = Renault
+    val ano = 2017
+    val modelo = Duster
+
+    constructor(marca: String, ano: Int, modelo: String){
+        this.marca = marca
+        this.ano = ano
+        this.modelo = modelo
+    }
+    fun medirCombustivel(){
+        var combustivel: Float
+
+        val gasolinamaxlitro = 10
+        val gasolinaminlitro = 1
+
+        if(combustivel == gasolinamaxlitro){
+            println("Gasolina do $modelo está em $gasolinamaxlitro, tanque está cheio")
+        }
+        else if(combustivel <= gasolinaminlitro){
+            println("O $modelo está com $gasolinaminlitro de gasolina, o tanque na reserva precisa abastecer!")
+        }
+    }
 }
